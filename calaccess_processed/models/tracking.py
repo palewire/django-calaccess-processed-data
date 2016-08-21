09d5edb6876c5593ab9635cbb7faa87b06a9150c
James Gordon <gordon.je@gmail.com>
2016-08-20 23:21:11 -0500

    Swap import of archive_directory_path from calaccess_raw to calaccess_processed

--- calaccess_processed/models/tracking.py
+++ calaccess_processed/models/tracking.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 from django.db import models
 from hurry.filesize import size as sizeformat
 from django.utils.encoding import python_2_unicode_compatible
-from calaccess_raw import archive_directory_path
+from calaccess_processed import archive_directory_path
 
 
 @python_2_unicode_compatible
