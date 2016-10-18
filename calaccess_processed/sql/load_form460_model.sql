04c6693331db7bd4a3a3cb0d40026114e92b6ae6
James Gordon <gordon.je@gmail.com>
2016-10-18 11:46:14 -0500

    Remove unnecessary aliases in SELECT clauses

--- calaccess_processed/sql/load_form460_model.sql
+++ calaccess_processed/sql/load_form460_model.sql
@@ -29,40 +29,40 @@ INSERT INTO calaccess_processed_form460 (
     outstanding_debts
 )
 SELECT 
-    f460.filing_id AS filing_id,
-    latest.amend_id AS amendment_count,
-    f460.filer_id AS filer_id,
-    f460.date_filed AS date_filed,
-    f460.from_date AS from_date,
-    f460.thru_date AS thru_date,
-    f460.filer_lastname AS filer_lastname,
-    f460.filer_firstname AS filer_firstname,
-    f460.election_date AS election_date,
-    f460.monetary_contributions AS monetary_contributions,
-    f460.loans_received AS loans_received,
-    f460.subtotal_cash_contributions AS subtotal_cash_contributions,
-    f460.nonmonetary_contributions AS nonmonetary_contributions,
-    f460.total_contributions AS total_contributions,
-    f460.payments_made AS payments_made,
-    f460.loans_made AS loans_made,
-    f460.subtotal_cash_payments AS subtotal_cash_payments,
-    f460.unpaid_bills AS unpaid_bills,
-    f460.nonmonetary_adjustment AS nonmonetary_adjustment,
-    f460.total_expenditures_made AS total_expenditures_made,
-    f460.begin_cash_balance AS begin_cash_balance,
-    f460.cash_receipts AS cash_receipts,
-    f460.miscellaneous_cash_increases AS miscellaneous_cash_increases,
-    f460.cash_payments AS cash_payments,
-    f460.ending_cash_balance AS ending_cash_balance,
-    f460.loan_guarantees_received AS loan_guarantees_received,
-    f460.cash_equivalents AS cash_equivalents,
-    f460.outstanding_debts AS outstanding_debts
+    f460.filing_id,
+    latest.amendment_count,
+    f460.filer_id,
+    f460.date_filed,
+    f460.from_date,
+    f460.thru_date,
+    f460.filer_lastname,
+    f460.filer_firstname,
+    f460.election_date,
+    f460.monetary_contributions,
+    f460.loans_received,
+    f460.subtotal_cash_contributions,
+    f460.nonmonetary_contributions,
+    f460.total_contributions,
+    f460.payments_made,
+    f460.loans_made,
+    f460.subtotal_cash_payments,
+    f460.unpaid_bills,
+    f460.nonmonetary_adjustment,
+    f460.total_expenditures_made,
+    f460.begin_cash_balance,
+    f460.cash_receipts,
+    f460.miscellaneous_cash_increases,
+    f460.cash_payments,
+    f460.ending_cash_balance,
+    f460.loan_guarantees_received,
+    f460.cash_equivalents,
+    f460.outstanding_debts
 FROM (
     -- get most recent amendment for each filing
-    SELECT filing_id, MAX(amend_id) AS amend_id
+    SELECT filing_id, MAX(amend_id) AS amendment_count
     FROM calaccess_processed_form460version
     GROUP BY 1
 ) AS latest
 JOIN calaccess_processed_form460version f460
 ON latest.filing_id = f460.filing_id
-AND latest.amend_id = f460.amend_id;
+AND latest.amendment_count = f460.amend_id;
--- calaccess_processed/sql/load_schedule497_model.sql
+++ calaccess_processed/sql/load_schedule497_model.sql
@@ -8,19 +8,19 @@ INSERT INTO calaccess_processed_schedule497 (
     election_date
 )
 SELECT
-    s497.filing_id AS filing_id,
-    latest.amend_id AS amendment_count,
-    s497.filer_id AS filer_id,
-    s497.date_filed AS date_filed,
-    s497.filer_lastname AS filer_lastname,
-    s497.filer_firstname AS filer_firstname,
-    s497.election_date AS election_date
+    s497.filing_id,
+    latest.amendment_count,
+    s497.filer_id,
+    s497.date_filed,
+    s497.filer_lastname,
+    s497.filer_firstname,
+    s497.election_date
 FROM (
     -- get most recent amendment for each filing
-    SELECT filing_id, MAX(amend_id) AS amend_id
+    SELECT filing_id, MAX(amend_id) AS amendment_count
     FROM calaccess_processed_schedule497version
     GROUP BY 1
 ) AS latest
 JOIN calaccess_processed_schedule497version s497
 ON latest.filing_id = s497.filing_id
-AND latest.amend_id = s497.amend_id;
+AND latest.amendment_count = s497.amend_id;
