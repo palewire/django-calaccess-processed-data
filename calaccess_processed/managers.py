0557178e5002fdd4a9906eb2971ba1aa99bea904
James Gordon <gordon.je@gmail.com>
2016-10-25 09:45:29 -0500

    Add properties/methods for dropping and re-adding model indexes

--- calaccess_processed/managers.py
+++ calaccess_processed/managers.py
@@ -5,30 +5,100 @@ Custom managers for working with CAL-ACCESS processed data models.
 """
 from __future__ import unicode_literals
 import os
-from django.db import models, connection
+from django.db import models, connection, migrations
 
 class ProcessedDataManager(models.Manager):
     """
     Utilities for loading raw CAL-ACCESS data into processed data models.
     """
+    def add_constraints_and_indexes(self):
+        with connection.schema_editor() as schema_editor:
+            schema_editor.alter_unique_together(
+                self.model,
+                (),
+                self.model._meta.unique_together,
+            )
+
+            schema_editor.alter_index_together(
+                self.model,
+                (),
+                self.model._meta.index_together,
+            )
+
+            for field in self.model.objects.constrained_fields:
+                field_copy = field.__copy__()
+                field_copy.db_constraint = False
+                schema_editor.alter_field(
+                    self.model, field_copy, field
+                )
+
+            for field in self.model.objects.indexed_fields:
+                field_copy = field.__copy__()
+                field_copy.db_index = False
+                schema_editor.alter_field(
+                    self.model, field_copy, field
+                )
+
+    def drop_constraints_and_indexes(self):
+        with connection.schema_editor() as schema_editor:
+            schema_editor.alter_unique_together(
+                self.model,
+                self.model._meta.unique_together,
+                (),
+            )
+
+            schema_editor.alter_index_together(
+                self.model,
+                self.model._meta.index_together,
+                (),
+            )
+
+            for field in self.model.objects.constrained_fields:
+                field_copy = field.__copy__()
+                field_copy.db_constraint = False
+                schema_editor.alter_field(
+                    self.model, field, field_copy
+                )
+
+            for field in self.model.objects.indexed_fields:
+                field_copy = field.__copy__()
+                field_copy.db_index = False
+                schema_editor.alter_field(
+                    self.model, field, field_copy
+                )
+
     def load_raw_data(self):
+        self.drop_constraints_and_indexes()
         with connection.cursor() as c:
             c.execute(self.raw_data_load_query)
+        self.add_constraints_and_indexes()
 
     @property
-    def raw_data_load_query_path(self):
-        return os.path.join(
-            os.path.dirname(__file__),
-            'sql',
-            'load_%s_model.sql' % self.model._meta.model_name,
-        )
-    
+    def constrained_fields(self):
+        return [
+            f for f in self.model._meta.fields
+            if hasattr(f, 'db_constraint') and f.db_constraint
+        ]
+
+    @property
+    def indexed_fields(self):
+        return [
+            f for f in self.model._meta.fields if f.db_index
+        ]
+
     @property
     def has_raw_data_load_query(self):
         if os.path.exists(self.raw_data_load_query_path):
             return True
         else:
             return False    
+
+    @property
+    def db_table(self):
+        """
+        Return the model's database table name as a string.
+        """
+        return self._meta.db_table
 
     @property
     def raw_data_load_query(self):
@@ -39,9 +109,9 @@ class ProcessedDataManager(models.Manager):
         return sql
 
     @property
-    def db_table(self):
-        """
-        Return the model's database table name as a string.
-        """
-        return self._meta.db_table
-
+    def raw_data_load_query_path(self):
+        return os.path.join(
+            os.path.dirname(__file__),
+            'sql',
+            'load_%s_model.sql' % self.model._meta.model_name,
+        )
