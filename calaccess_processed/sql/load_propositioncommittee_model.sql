041e15f29423a1ab969df63640b9e1c982a9bea5
James Gordon <gordon.je@gmail.com>
2016-12-05 13:35:38 -0600

    Add semicolons between separate sql statements PR #18

--- calaccess_processed/sql/load_propositioncommittee_model.sql
+++ calaccess_processed/sql/load_propositioncommittee_model.sql
@@ -7,7 +7,7 @@ SELECT
 	committee."scraped_id"::INTEGER AS id,
 	MIN(committee."name") AS name
 FROM "calaccess_processed_scrapedpropositioncommittee" AS committee
-GROUP BY committee."scraped_id"
+GROUP BY committee."scraped_id";
 
 -- Add links between committees and propositions they support and oppose
 INSERT INTO calaccess_processed_propositionsupportoppose (
@@ -21,4 +21,4 @@ SELECT
 	left(committee."position",1) AS support_oppose
 FROM "calaccess_processed_scrapedpropositioncommittee" AS committee
 JOIN "calaccess_processed_scrapedproposition" AS proposition
-ON committee."proposition_id" = proposition."id"
+ON committee."proposition_id" = proposition."id";
