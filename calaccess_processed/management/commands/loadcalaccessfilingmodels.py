9971c76d046a4aa408e47d8b138de7bd4f7b04cd
James Gordon <gordon.je@gmail.com>
2017-05-25 12:04:50 -0500

    DRYer code, also use the model object name ProcessedDataFile.file_name

--- calaccess_processed/management/commands/loadcalaccessfilingmodels.py
+++ calaccess_processed/management/commands/loadcalaccessfilingmodels.py
@@ -5,15 +5,10 @@ Load and archive the CAL-ACCESS Filing and FilingVersion models.
 """
 from django.apps import apps
 from django.core.management import call_command
-from django.core.management.base import CommandError
 from django.db import connection
 from django.utils.timezone import now
-from calaccess_raw.models.tracking import RawDataVersion
 from calaccess_processed.management.commands import CalAccessCommand
-from calaccess_processed.models.tracking import (
-    ProcessedDataVersion,
-    ProcessedDataFile,
-)
+from calaccess_processed.models.tracking import ProcessedDataFile
 
 
 class Command(CalAccessCommand):
@@ -22,71 +17,119 @@ class Command(CalAccessCommand):
     """
     help = 'Load and archive the CAL-ACCESS Filing and FilingVersion models.'
 
+    def add_arguments(self, parser):
+        """
+        Adds custom arguments specific to this command.
+        """
+        parser.add_argument(
+            "--force-restart",
+            "--restart",
+            action="store_true",
+            dest="restart",
+            default=False,
+            help="Force re-start (overrides auto-resume)."
+        )
+
     def handle(self, *args, **options):
         """
         Make it happen.
         """
         super(Command, self).handle(*args, **options)
 
-        # get the most recently loaded raw data version
-        try:
-            self.raw_version = RawDataVersion.objects.complete()[0]
-        except IndexError:
-            raise CommandError(
-                'No raw CAL-ACCESS data loaded (run `python manage.py '
-                'updatecalaccessrawdata`).'
-            )
+        self.force_restart = options.get("restart")
 
         # get or create the ProcessedDataVersion instance
-        self.processed_version, created = ProcessedDataVersion.objects.get_or_create(
-            raw_version=self.raw_version,
-        )
-        # log if starting or resuming
-        if created:
+        self.processed_version, created = self.get_or_create_processed_version()
+
+        if self.processed_version.files.count() > 0 and not self.force_restart:
             self.header(
-                'Load filings from {:%m-%d-%Y %H:%M:%S} snapshot'.format(
-                    self.raw_version.release_datetime
+                'Resume loading of filings from {:%m-%d-%Y %H:%M:%S} snapshot'.format(
+                    self.processed_version.raw_version.release_datetime,
                 )
             )
         else:
             self.header(
-                'Resume loading of filings from {:%m-%d-%Y %H:%M:%S} snapshot'.format(
-                    self.raw_version.release_datetime
+                'Load filings from {:%m-%d-%Y %H:%M:%S} snapshot'.format(
+                    self.processed_version.raw_version.release_datetime
                 )
             )
-        # if there isn't already a process start datetime, set it
-        if not self.processed_version.process_start_datetime:
+        # set the time if not there already or forcing restart
+        if not self.processed_version.process_start_datetime or self.force_restart:
             self.processed_version.process_start_datetime = now()
             self.processed_version.save()
 
-        version_models = [
-            m for m in apps.get_app_config('calaccess_processed').get_models()
-            if not m._meta.abstract and
-            'filings' in str(m) and
-            'Version' in str(m)
-        ]
-        if self.verbosity >= 2:
-            self.log(
-                " Loading filing %s verison models." % len(version_models)
-            )
+        # handle version models first
+        version_models = self.get_model_list('version')
         self.load_model_list(version_models)
 
-        filing_models = [
-            m for m in apps.get_app_config('calaccess_processed').get_models()
-            if not m._meta.abstract and
-            'filings' in str(m) and
-            'Version' not in str(m)
-        ]
-        if self.verbosity >= 2:
-            self.log(
-                " Loading filing %s models." % len(filing_models)
-            )
+        # then filing models
+        filing_models = self.get_model_list('filing')
         self.load_model_list(filing_models)
 
         self.processed_version.process_finish_datetime = now()
         self.processed_version.save()
 
         self.success("Done!")
+
+    def get_model_list(self, model_type):
+        """
+        Return a list of models of the specified type to be loaded.
+
+        model_type must be "version" of "filing".
+        """
+        non_abstract_models = [
+            m for m in apps.get_app_config('calaccess_processed').get_models()
+            if not m._meta.abstract and
+            'filings' in str(m)
+        ]
+
+        if model_type == 'version':
+            models_to_load = [
+                m for m in non_abstract_models if 'Version' in str(m)
+            ]
+        elif model_type == 'filing':
+            models_to_load = [
+                m for m in non_abstract_models if 'Version' not in str(m)
+            ]
+        else:
+            raise Exception('model_type must be "version" or "filing".')
+
+        # if not forcing a restart, filter out the models already loaded
+        if not self.force_restart:
+            loaded_models_q = ProcessedDataFile.objects.filter(
+                version=self.processed_version,
+                process_finish_datetime__isnull=False,
+            )
+            if model_type == 'version':
+                loaded_models_q.filter(file_name__icontains='Version')
+            elif model_type == 'filing':
+                loaded_models_q.filter(
+                    file_name__icontains='Form'
+                ).exclude(file_name__icontains='Version')
+
+            loaded_models = [i.file_name for i in loaded_models_q.all()]
+
+            if self.verbosity >= 2:
+                self.log(
+                    " {0} {1} models already loaded.".format(
+                        len(loaded_models),
+                        model_type,
+                    )
+                )
+            models_to_load = [
+                m for m in models_to_load
+                if m._meta.object_name not in loaded_models
+            ]
+
+        if self.verbosity >= 2:
+            self.log(
+                " Loading {0} {1} models.".format(
+                    len(models_to_load),
+                    model_type,
+                )
+            )
+
+        return models_to_load
 
     def load_model_list(self, model_list):
         """
@@ -97,7 +140,7 @@ class Command(CalAccessCommand):
             # set up the ProcessedDataFile instance
             processed_file, created = ProcessedDataFile.objects.get_or_create(
                 version=self.processed_version,
-                file_name=m._meta.model_name,
+                file_name=m._meta.object_name,
             )
             processed_file.process_start_datetime = now()
             processed_file.save()
