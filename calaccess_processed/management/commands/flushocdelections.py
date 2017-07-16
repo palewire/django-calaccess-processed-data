bad4bef06881918be42fbeed916ffd3031d44aab
palewire <ben.welsh@gmail.com>
2017-07-16 09:21:13 -0700

    Drafted a flush command

---
+++ calaccess_processed/management/commands/flushocdelections.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Flush data from OCD models.
+"""
+from opencivicdata.core.models import (
+    Membership,
+    Organization,
+    Person,
+    Post,
+)
+from opencivicdata.elections.models import (
+    Candidacy,
+    CandidateContest,
+    Election,
+    BallotMeasureContest,
+    RetentionContest
+)
+from calaccess_processed.management.commands import CalAccessCommand
+
+
+class Command(CalAccessCommand):
+    """
+    Flush data from OCD models.
+    """
+    help = "Flush data from OCD models."
+
+    def handle(self, *args, **options):
+        super(Command, self).handle(*args, **options)
+
+        # Flush models
+        qs_list = [
+            Candidacy.objects.all(),
+            CandidateContest.objects.all(),
+            BallotMeasureContest.objects.all(),
+            RetentionContest.objects.all(),
+            Election.objects.all(),
+            Membership.objects.all(),
+            Person.objects.all(),
+            Post.objects.all(),
+            Organization.objects.filter(classification='party')
+        ]
+        for qs in qs_list:
+            if self.verbosity > 0:
+                self.log("Flushing {} {} objects".format(qs.count(), qs.model.__name__))
+            qs.delete()
