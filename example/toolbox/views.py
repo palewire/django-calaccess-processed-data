5779f6efd1098f1a354bcdc0a2bbd120d01fa335
palewire <ben.welsh@gmail.com>
2017-07-25 09:34:48 -0700

    Sorting

--- example/toolbox/views.py
+++ example/toolbox/views.py
@@ -6,9 +6,10 @@ from calaccess_processed.models import OCDElectionProxy
 
 def election_list(request):
     object_list = OCDElectionProxy.objects.all()
-    group_list = defaultdict()
+    group_dict = defaultdict()
     for obj in object_list:
-        group_list.setdefault(obj.date.year, []).append(obj)
+        group_dict.setdefault(obj.date.year, []).append(obj)
+    group_list = sorted(group_dict.items(), key=lambda x:x[0], reverse=True)
     context = dict(object_list=object_list, group_list=group_list)
     template = "election_list.html"
     return render(request, template, context)
