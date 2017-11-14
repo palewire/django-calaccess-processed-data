ea49530f4477462c48155e6ad14882f0530e2791
James Gordon <gordon.je@gmail.com>
2017-11-14 10:07:17 -0600

    remove unused sql snippets

--- calaccess_processed/sql/join_filingversion_to_filing.sql
+++
@@ -1,3 +0,0 @@
--- join FilingVersion model table to Filing model table
-JOIN {rhs_table}
-ON {lhs_table}."filing_version_id" = {rhs_table}."id"
--- calaccess_processed/sql/join_to_committee_identifier.sql
+++
@@ -1,4 +0,0 @@
--- join to CommitteeIdentifier to get OCD committee_id
-JOIN "opencivicdata_committeeidentifier"
-ON {lhs_table}."filer_id" = "opencivicdata_committeeidentifier"."identifier"::int
-AND "opencivicdata_committeeidentifier"."scheme" = 'calaccess_filer_id'
